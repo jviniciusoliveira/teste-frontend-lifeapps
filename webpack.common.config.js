95a2613b6367ee9a10b05fc00f94277e7599d7c0
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-14 20:58:19 -0300

    chore: aplica formatação do `prettier`

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,25 +1,25 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "main-[contenthash].js",
-    publicPath: "/",
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-[contenthash].js',
+    publicPath: '/',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      "@": path.join(__dirname, "src"),
+      '@': path.join(__dirname, 'src'),
     },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
     }),
   ],
-};
+}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,21 +1,21 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.config");
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.config')
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
     historyApiFallback: true,
     compress: true,
     port: 3000,
   },
-});
+})
