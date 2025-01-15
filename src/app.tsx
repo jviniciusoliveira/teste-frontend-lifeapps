2c5d47a9ba3b8920a543e379032cfa9c9c008643
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-15 11:35:35 -0300

    feat: persiste a `store` no localstorage com `redux-persist`

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
 import { Provider as StoreProvider } from 'react-redux'
+import { PersistGate as PersistGateProvider } from 'redux-persist/integration/react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 
 import { Home } from '@/pages/home'
-import { store } from '@/store/setup'
+import { store, persistor } from '@/store'
 
 export function App() {
   return (
     <StoreProvider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGateProvider persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGateProvider>
     </StoreProvider>
   )
 }
--- src/store/setup.ts
+++ src/store/setup.ts
@@ -1,11 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import cartReducer from './cart'
+
+const persistedReducer = persistReducer(
+  {
+    key: 'cart',
+    storage,
+  },
+  cartReducer
+)
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
