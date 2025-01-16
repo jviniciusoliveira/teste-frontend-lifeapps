cb28a6502d0a5c8c6ff7dd241a9c71d729621f8c
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-16 15:52:26 -0300

    refactor: adiciona componente `CardProduct`

---
+++ src/components/card-product/index.tsx
@@ -0,0 +1,39 @@
+import styles from './styles.module.css'
+
+import { Card } from '@/components/ui/card'
+import { Product } from '@/store'
+
+type CardProductProps = {
+  product: Product
+}
+
+export function CardProduct({ product }: CardProductProps) {
+  return (
+    <Card to={`/product/${product.id}`}>
+      <Card.Image>
+        {Boolean(product.discount_percentage) && (
+          <span className={styles.productDiscount}>
+            -{product.discount_percentage}%
+          </span>
+        )}
+        <img src={product.image} alt={product.description} />
+      </Card.Image>
+      <Card.Content>
+        <h3 className={styles.productName}>{product.name}</h3>
+        <h4 className={styles.productDescription}>{product.description}</h4>
+        <div className={styles.productPriceContainer}>
+          {Boolean(product.promotional_price) && (
+            <strong className={styles.productPrice}>
+              R$ {product.promotional_price},00
+            </strong>
+          )}
+          <strong
+            className={`${styles.productPrice} ${product.promotional_price ? styles.productWithPromotionalPrice : ''}`}
+          >
+            R$ {product.price},00
+          </strong>
+        </div>
+      </Card.Content>
+    </Card>
+  )
+}
--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 import { Pagination } from '@/components/pagination'
 import { useProductsList } from '@/hooks/useProductsList'
 import { useProductsSearch } from '@/hooks/useProductsSearch'
-import { Card } from '@/components/ui/card'
+import { CardProduct } from '../card-product'
 
 export function Products() {
   const { searchByPage } = useProductsSearch()
@@ -13,34 +13,7 @@ export function Products() {
     <>
       <div className={styles.container}>
         {products.map((product) => (
-          <Card to={`/product/${product.id}`}>
-            <Card.Image>
-              {Boolean(product.discount_percentage) && (
-                <span className={styles.productDiscount}>
-                  -{product.discount_percentage}%
-                </span>
-              )}
-              <img src={product.image} alt={product.description} />
-            </Card.Image>
-            <Card.Content>
-              <h3 className={styles.productName}>{product.name}</h3>
-              <h4 className={styles.productDescription}>
-                {product.description}
-              </h4>
-              <div className={styles.productPriceContainer}>
-                {Boolean(product.promotional_price) && (
-                  <strong className={styles.productPrice}>
-                    R$ {product.promotional_price},00
-                  </strong>
-                )}
-                <strong
-                  className={`${styles.productPrice} ${product.promotional_price ? styles.productWithPromotionalPrice : ''}`}
-                >
-                  R$ {product.price},00
-                </strong>
-              </div>
-            </Card.Content>
-          </Card>
+          <CardProduct key={product.id} product={product} />
         ))}
       </div>
 
