2bd71d595f4f5bf36116b0f6bfe6de6043366f68
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-16 15:56:20 -0300

    refactor: move conteúdo de `products` para página `Home`

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,32 @@
+import styles from './styles.module.css'
+
+import { CardProduct } from '@/components/card-product'
 import { Categories } from '@/components/categories'
-import { Products } from '@/components/products'
+import { Pagination } from '@/components/pagination'
+import { useProductsList } from '@/hooks/useProductsList'
+import { useProductsSearch } from '@/hooks/useProductsSearch'
 
 export function Home() {
+  const { searchByPage } = useProductsSearch()
+  const { products, totalPages, currentPage } = useProductsList()
+
   return (
     <>
       <Categories />
-      <Products />
+
+      <div className={styles.productListContainer}>
+        {products.map((product) => (
+          <CardProduct key={product.id} product={product} />
+        ))}
+      </div>
+
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={searchByPage}
+        />
+      )}
     </>
   )
 }
