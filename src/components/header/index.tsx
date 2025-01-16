9b0aaca985f03f8db0388d546bd5d03e1c7d8579
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-15 21:31:25 -0300

    feat: adiciona pesquisa por nome do produto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,18 +7,41 @@ import BrandIcon from '@/assets/icons/brand.svg'
 import FavoriteIcon from '@/assets/icons/favorite.svg'
 import SearchIcon from '@/assets/icons/search.svg'
 import UserIcon from '@/assets/icons/user.svg'
+import { useRef } from 'react'
+import { useProducts } from '@/hooks/useProducts'
 
 export function Header() {
+  const { getByName } = useProducts()
+  const formRef = useRef(null) as React.MutableRefObject<HTMLFormElement>
+
+  async function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    const inputText = formData.get('input-search') as string
+
+    if (!inputText?.trim()) return
+
+    getByName(inputText)
+    formRef.current.reset()
+  }
+
   return (
     <header className={styles.container}>
       <Link to="/" className={styles.brandContainer} title="Home">
         <BrandIcon className={styles.brand} />
         <h1 className={styles.title}>E-Commerce</h1>
       </Link>
-      <div className={styles.searchInput}>
-        <input placeholder="Buscar produtos" />
-        <SearchIcon />
-      </div>
+      <form
+        ref={formRef}
+        onSubmit={(e) => handleSearch(e)}
+        className={styles.searchInput}
+      >
+        <input type="text" name="input-search" placeholder="Buscar produtos" />
+        <button type="submit" className={styles.searchSubmit}>
+          <SearchIcon />
+        </button>
+      </form>
       <div className={styles.actions}>
         <Link to="/cart">
           <CartIcon />
