e1e605cf10b98806b260615b1da6e4743828d2e6
Jose Vinicius <jviniciusoliveira@gmail.com>
2025-01-15 21:13:56 -0300

    feat: adiciona ações nos filtros de categoria

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,16 +1,41 @@
 import styles from './styles.module.css'
+import { useProducts } from '@/hooks/useProducts'
 
 export function Categories() {
+  const { getByCategory, getByPage } = useProducts()
+
+  function handleGetByCategory(category: string) {
+    return function () {
+      getByCategory(category)
+    }
+  }
+
   return (
     <nav className={styles.container}>
       <button
+        onClick={() => getByPage(1)}
         className={`${styles.categoryOption} ${styles.categoryOptionActive}`}
       >
         Todos os produtos
       </button>
-      <button className={styles.categoryOption}>Camisetas</button>
-      <button className={styles.categoryOption}>Calças</button>
-      <button className={styles.categoryOption}>Tênis</button>
+      <button
+        onClick={handleGetByCategory('Camisetas')}
+        className={styles.categoryOption}
+      >
+        Camisetas
+      </button>
+      <button
+        onClick={handleGetByCategory('Calças')}
+        className={styles.categoryOption}
+      >
+        Calças
+      </button>
+      <button
+        onClick={handleGetByCategory('Tênis')}
+        className={styles.categoryOption}
+      >
+        Tênis
+      </button>
     </nav>
   )
 }
